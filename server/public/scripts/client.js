e99f129ceb6c8d4d0a0c9068b374787790ce38a9
Jacob Albright <jacob.albright23@gmail.com>
2018-02-24 12:17:59 -0600

    Client-side comment functionality works as far as displaying comment-input fields and submitting. Server sends and retrieves comments successfully for corrosponding images. NEXT, display comments in a good way

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,27 +4,10 @@ const galleryController = app.controller('galleryController', ['$http', galleryC
 
 function galleryCtl($http){
     let self = this;
+    self.imagesArray = []; // This will hold the image objects that come back from the GET request in self.getImages
 
-    self.imagesArray = [];
-
-    self.getImages = function(){
-        $http({
-            method: 'GET',
-            url: '/gallery',
-        }).then(function(response){
-            console.log(response.data);
-            self.imagesArray = response.data
-            for (let pic of self.imagesArray) {
-                pic.clicked = false;
-                pic.commenting = false;
-            }
-        }).catch(function(error){
-            console.log(error);            
-        }); // END $http
-    }; // END self.getImages
-
-    self.imgClick = function(image){
-        if(image.clicked === false){
+    self.imgClick = function (image) {
+        if (image.clicked === false) {
             for (let pic of self.imagesArray) {
                 pic.clicked = false;
             }
@@ -34,6 +17,48 @@ function galleryCtl($http){
         }
     }
 
+    self.commentBtnClick = (image) => {
+        if (image.commenting === false) {
+            for (let pic of self.imagesArray) {
+                pic.commenting = false;
+            }
+            image.commenting = true
+        } else {
+            image.commenting = false;
+        }
+    }
+
+    self.getImages = function(){
+        $http({
+            method: 'GET',
+            url: '/gallery',
+        }).then(function(response){
+            console.log(response.data);
+            self.getComments();
+            self.imagesArray = response.data
+            for (let pic of self.imagesArray) {
+                pic.clicked = false;
+                pic.commenting = false;
+                pic.comment = '';
+            }
+        }).catch(function(error){
+            console.error(error);            
+        }); // END $http
+    }; // END self.getImages
+
+    self.getComments = function(){
+        $http({
+            method: 'GET',
+            url: '/gallery/comments'
+        }).then(function(response){
+            console.log(response);
+            // get the response into self.imagesArray
+        }).catch(function(error){
+            console.error(error);
+        })
+    }
+
+    
     self.likeClick = (image) => {
         image.up_votes++;
         $http({
@@ -43,21 +68,25 @@ function galleryCtl($http){
         }).then(function(response){
             console.log(response);
         }).catch(function(error){
-            console.log(error);   
+            console.error(error);   
             image.up_votes--;
         }); // END $http
     } // END self.likeClick
 
-    self.commentClick = (image) => {
-        if(image.commenting === false){
-            for(let pic of self.imagesArray){
-                pic.commenting = false;
-            }
-            image.commenting = true
-        } else {
-            image.commenting = false;
-        }
-    }
+  
+    self.submitComment = (image) => {
+        $http({
+            method: 'POST',
+            url: '/gallery/comment',
+            data: {comment: image.comment, id: image.id}
+        }).then(function(response){
+            self.getImages();
+        }).catch(function(error){
+            console.error(error);
+        }); // END $http
+        image.comment = ''; // clear the comment field
+    } // END self.submitComment
+
 
     // ON LOAD
     self.getImages(); 
--- server/routes/gallery-router.js
+++ server/routes/gallery-router.js
@@ -13,6 +13,17 @@ router.get('/', (req, res) => {
     });
 }); // END router /gallery GET
 
+router.get('/comments', (req, res) => {
+    const sqlText = ` SELECT comment_id, picture_id, comment FROM comments 
+ 	JOIN pictures_comments ON comments.id = pictures_comments.comment_id;`;
+    pool.query(sqlText).then(function(response){
+        res.send(response.rows);
+    }).catch(function(error){
+        console.log('PROBLEM IN ROUTER.GET /GALLERY/COMMENTS', error);
+        res.sendStatus(500);
+    }); // END pool.query
+}); // END router /gallery/comments GET
+
 router.put('/likes/:id', (req, res) => {
     const id = req.params.id;
     const likes = req.body.likes;
@@ -25,6 +36,29 @@ router.put('/likes/:id', (req, res) => {
     }); // END pool.query
 }); // END router /gallery/likes PUT
 
+router.post('/comment', (req, res) => {
+    const id = req.body.id;
+    const comment = req.body.comment;
+    const sqlText = `INSERT INTO comments (comment) 
+    VALUES ($1);`;
+    pool.query(sqlText, [comment]).then(function(response){
+        pictures_commentsINSERT(id, comment)
+        res.sendStatus(200)
+    }).catch(function(error){
+        console.log('PROBLEM IN ROUTER.POST /GALLERY/COMMENT');
+        res.sendStatus(500);
+    }); // END pool.query
+}); // END router /gallery/comment POST
+
+function pictures_commentsINSERT(id, comment){
+    const sqlText = `INSERT INTO pictures_comments (picture_id, comment_id)
+    VALUES ($1, (SELECT id FROM comments WHERE comment=$2));`;
+    pool.query(sqlText, [id, comment]).then(function(response){
+        res.sendStatus(200);
+    }).catch(function(error){
+        console.log('PROBLEM IN pictures_commentsINSERT', error);
+    }); // END pool.query
+} // END pictures_commentsINSERT
 
 
 
