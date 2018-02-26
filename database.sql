a33c4b54b2da2c1ad8177926c585858e10c41c88
Jacob Albright <jacob.albright23@gmail.com>
2018-02-25 19:37:41 -0600

    not much...

--- database.sql
+++ database.sql
@@ -1,25 +1,30 @@
-CREATE TABLE pictures (
+CREATE TABLE pictures
+(
 	id SERIAL PRIMARY KEY,
 	url VARCHAR,
 	synopsis VARCHAR,
-	up_votes INT
+	up_votes INT,
+	view_count INT
 );
 
-CREATE TABLE comments (
+CREATE TABLE comments
+(
 	id SERIAL PRIMARY KEY,
-	comment VARCHAR,
-	user_id INT
+	comment VARCHAR
 );
 
-CREATE TABLE pictures_comments (
+CREATE TABLE users
+(
+	id SERIAL PRIMARY KEY,
+	username VARCHAR
+);
+
+
+CREATE TABLE pictures_comments_users
+(
 	id SERIAL PRIMARY KEY,
 	picture_id INT references pictures,
 	comment_id INT references comments,
 	user_id INT references users
- );
- 
- CREATE TABLE users (
- 	id SERIAL PRIMARY KEY,
- 	username VARCHAR
- );
+);
  
