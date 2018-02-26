37aa2ec19e2b05fa2a7a939816fdb25538cb62c0
Jacob Albright <jacob.albright23@gmail.com>
2018-02-25 22:51:53 -0600

    updated database.sql with some INSERT values

--- database.sql
+++ database.sql
@@ -28,3 +28,14 @@ CREATE TABLE pictures_comments_users
 	user_id INT references users
 );
  
+
+INSERT INTO pictures
+	(url, synopsis, up_votes, view_count )
+VALUES
+	('./images/door-richard-1.png', 'This is Richard Parker. He is the neighbor cat. He comes to our door sometimes.', 0, 0),
+	('./images/door-richard-2.png', 'Richard Parker will go into anyone''s house.', 0, 0),
+	('./images/may-day-1.png', 'These are three adults standing around before the May Day parade.', 0, 0),
+	('./images/door-richard-3.png', 'Richard Parker is very friendly and asks everyone for belly rubs.', 0, 0),
+	('./images/may-day-3.png', 'This is also pre May Day parade. The May Day parade is wicked sweet!', 0, 0),
+	('./images/may-day-2.png', 'These are some more crazy kids at the May Day parade.', 0, 0);
+ 
